92eeeaaff72cc0cb149eceba9780c03c9b973912
Gpbhupinder <60905045+gpbhupinder@users.noreply.github.com>
2024-01-27 20:02:38 +0530

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  build: { 
+    
+    assets: '_assets',
+    
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'entry.[hash].mjs',
+          chunkFileNames: 'chunks/chunk.[hash].mjs',
+          assetFileNames: 'assets/asset.[hash][extname]',
+        },
+      },
+    },
+  },
+  integrations: [tailwind(), relativeLinks()]
+});
