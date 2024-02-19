51fd2df6f7d522f39f0643d35c79de2df0ccbbd7
Gpbhupinder <60905045+gpbhupinder@users.noreply.github.com>
2024-02-19 14:54:08 +0530

    font, terms, privacy

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				merriweather: ['Merriweather', ...defaultTheme.fontFamily.sans],
+			  },
 			colors: {
 				primary: "var(--primary-color)",
 				primaryhover: "var(--primary-color-hover)",
 				secondary: "var(--secondary-color)",
 				secondaryhover: "var(--secondary-color-hover)",
 				tertiary: "var(--tertiary-color)",
+				lightbg: "var(--light-bg)",
 			  },
 		},
 	},
